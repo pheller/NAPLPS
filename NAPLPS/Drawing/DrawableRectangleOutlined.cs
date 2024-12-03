1e5ebe78c99ced51dfca3a8b3f88c0db6da8e9be
Andrew Portner <aportner@gmail.com>
2024-12-03 15:19:12 -0800

    rectangle outlined

--- NAPLPS/Drawing/DrawableRectangleOutlined.cs
+++ NAPLPS/Drawing/DrawableRectangleOutlined.cs
@@ -8,30 +8,30 @@ using Rectangle = SixLabors.ImageSharp.Rectangle;
 
 namespace NAPLPS.Drawing;
 
-public class DrawableRectangleOutlined : IDrawable
+public class DrawableRectangleOutlined : Drawable, IDrawable
 {
     private readonly RectangleOutlinedCommand _command;
 
-    public DrawableRectangleOutlined(RectangleOutlinedCommand command)
+    public DrawableRectangleOutlined(RectangleOutlinedCommand command) : base(command)
     {
         _command = command;
     }
 
     public void Draw(Image<Rgba32> image, NaplpsState state, Size size)
     {
-        var polygonPoints = new List<PointF>();
+        var startPoint = _command.Points.FirstOrDefault();
+        var dimensions = _command.Dimensions;
 
-        var startPointF = _command.Points.FirstOrDefault();
+        var (x1, y1, x2, y2) = NaplpsUtils.ConvertRectToScreen(
+            size,
+            startPoint.X,
+            startPoint.Y,
+            dimensions.X,
+            dimensions.Y
+        );
 
-        var startPoint = NaplpsUtils.ConvertNormalizedToPoint(size, startPointF.X, startPointF.Y);
-
-        var fgcolor = state.ColorMode == 0 ? state.Foreground.ToColor() : state.ColorMap[state.ColorMapForeground].ToColor();
-
-        var dimensions = NaplpsUtils.ConvertNormalizedToPoint(size, _command.Dimensions.X, _command.Dimensions.Y);
-
-        var rect = new Rectangle(startPoint.X, startPoint.Y, dimensions.X, dimensions.Y);
-
-        var pen = Pens.Solid(fgcolor.ToISColor(), 1f);
+        var (brush, pen) = GetBrushAndPenFromFillableCommand(size);
+        var rect = new Rectangle(x1, y1, x2, y2);
 
         image.Mutate(x => x.Draw(pen, rect));
     }
